6f81053b42049a0c899fb29ca98a8fb3525e629d
Luis Altamirano Urovo <luisjoseaj3@gmail.com>
2021-04-21 15:36:19 +0800

    Create Profile UI

---
+++ lib/ui_resources/textfield_styles.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+import 'custom_colors.dart';
+
+OutlineInputBorder textfieldBorder = OutlineInputBorder(
+borderRadius: BorderRadius.circular(10),
+borderSide: BorderSide(
+color: mainColor, width: 2));
