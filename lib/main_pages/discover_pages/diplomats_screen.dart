74b454cf223fb0a487f3602ed650d37c315f9956
ihasivony <ihasivony@yahoo.fr>
2021-07-12 19:36:45 +0800

    architect done

--- lib/main_pages/discover_pages/diplomats_screen.dart
+++ lib/main_pages/discover_pages/diplomats_screen.dart
@@ -9,141 +9,141 @@ import 'package:matched_app/resources/major_buttom.dart';
 import 'package:matched_app/ui_resources/custom_colors.dart';
 
 class DiplomatsScreen extends StatelessWidget {
-  const DiplomatsScreen({Key key,}) : super(key: key);
+  const DiplomatsScreen({
+    Key key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     ScreenScaler scaler = ScreenScaler()..init(context);
     Color color = Color(0xffB5fc89);
-      return Scaffold(
-        backgroundColor: dark,
-        body: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.only(top: scaler.getHeight(3)),
-                child: Row(
-                  children: <Widget>[
-                    //Buttom for back
-                    Container(
-                      child: IconButton(
-                        icon: Icon(
-                          Ionicons.ios_arrow_back,
-                          color: mainColor,
-                          size: 30,
-                        ),
-                        onPressed: () => Navigator.of(context).pop(),
+    return Scaffold(
+      backgroundColor: dark,
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(top: scaler.getHeight(3)),
+              child: Row(
+                children: <Widget>[
+                  //Buttom for back
+                  Container(
+                    child: IconButton(
+                      icon: Icon(
+                        Ionicons.ios_arrow_back,
+                        color: mainColor,
+                        size: 30,
+                      ),
+                      onPressed: () => Navigator.of(context).pop(),
+                    ),
+                  ),
+                  //Title Text
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    child: FittedBox(
+                      fit: BoxFit.contain,
+                      child: Text(
+                        '''Diplomats''',
+                        style: GoogleFonts.lato(
+                            textStyle: TextStyle(
+                                fontSize: 36,
+                                color: white,
+                                fontWeight: FontWeight.bold)),
                       ),
                     ),
-                    //Title Text
-                    Container(
-                      alignment: Alignment.centerLeft,
-                      child: FittedBox(
-                        fit: BoxFit.contain,
-                        child: Text(
-                          '''Diplomats''',
-                          style: GoogleFonts.lato(
-                              textStyle: TextStyle(
-                                  fontSize: 36,
-                                  color: white,
-                                  fontWeight: FontWeight.bold)),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-              //CSE Button
-              Padding(
-                padding: EdgeInsets.only(
-                    top: scaler.getHeight(1),
-                    bottom: scaler.getHeight(1),
-                    left: scaler.getHeight(1.5),
-                    right: scaler.getHeight(1.5)),
-                child: MajorButtom(
-                  detail: """Advocate
+            ),
+            //CSE Button
+            Padding(
+              padding: EdgeInsets.only(
+                  top: scaler.getHeight(1),
+                  bottom: scaler.getHeight(1),
+                  left: scaler.getHeight(1.5),
+                  right: scaler.getHeight(1.5)),
+              child: MajorButtom(
+                detail: """Advocate
 (INFJ)"""
-                      .trim(),
-                  image: PersonalityTestInfo().personalityIconsPath['INFJ'],
-                  alignment: Alignment(-.3, 0),
-                  color: color,
-                  screen: PersonalityTemplate(
-                    desc: CollegesInfo().shawDesc,
-                    imagesList: CollegesInfo().defaultList,
-                    title: """Advocate (INFJ)""",
-                  ),
+                    .trim(),
+                image: PersonalityTestInfo().personalityIconsPath['INFJ'],
+                alignment: Alignment(-.3, 0),
+                color: color,
+                screen: PersonalityTemplate(
+                  desc: CollegesInfo().shawDesc,
+                  imagesList: CollegesInfo().defaultList,
+                  title: """Advocate (INFJ)""",
                 ),
               ),
-              //EIE Button
-              Padding(
-                padding: EdgeInsets.only(
-                    top: scaler.getHeight(1),
-                    bottom: scaler.getHeight(1),
-                    left: scaler.getHeight(1.5),
-                    right: scaler.getHeight(1.5)),
-                child: MajorButtom(
-                  detail: """Mediator
+            ),
+            //EIE Button
+            Padding(
+              padding: EdgeInsets.only(
+                  top: scaler.getHeight(1),
+                  bottom: scaler.getHeight(1),
+                  left: scaler.getHeight(1.5),
+                  right: scaler.getHeight(1.5)),
+              child: MajorButtom(
+                detail: """Mediator
 (INFP)"""
-                      .trim(),
-                  image: PersonalityTestInfo().personalityIconsPath['INFP'],
-                  color: color,
-                  alignment: Alignment(-.3, 0),
-                  screen: PersonalityTemplate(
-                    desc: CollegesInfo().shawDesc,
-                    imagesList: CollegesInfo().defaultList,
-                    title: """Mediator (INFP)""",
-                  ),
+                    .trim(),
+                image: PersonalityTestInfo().personalityIconsPath['INFP'],
+                color: color,
+                alignment: Alignment(-.3, 0),
+                screen: PersonalityTemplate(
+                  desc: CollegesInfo().shawDesc,
+                  imagesList: CollegesInfo().defaultList,
+                  title: """Mediator (INFP)""",
                 ),
               ),
-              //NES Button
-              Padding(
-                padding: EdgeInsets.only(
-                    top: scaler.getHeight(1),
-                    bottom: scaler.getHeight(1),
-                    left: scaler.getHeight(1.5),
-                    right: scaler.getHeight(1.5)),
-                child: MajorButtom(
-                  detail: """Protagonist
+            ),
+            //NES Button
+            Padding(
+              padding: EdgeInsets.only(
+                  top: scaler.getHeight(1),
+                  bottom: scaler.getHeight(1),
+                  left: scaler.getHeight(1.5),
+                  right: scaler.getHeight(1.5)),
+              child: MajorButtom(
+                detail: """Protagonist
 (ENFJ)"""
-                      .trim(),
-                  color: color,
-                  image: PersonalityTestInfo().personalityIconsPath['ENFJ'],
-                  alignment: Alignment(-.3, 0),
-                  screen: PersonalityTemplate(
-                    desc: CollegesInfo().shawDesc,
-                    imagesList: CollegesInfo().defaultList,
-                    title: """Protagonist (ENFJ)""",
-                  ),
+                    .trim(),
+                color: color,
+                image: PersonalityTestInfo().personalityIconsPath['ENFJ'],
+                alignment: Alignment(-.3, 0),
+                screen: PersonalityTemplate(
+                  desc: CollegesInfo().shawDesc,
+                  imagesList: CollegesInfo().defaultList,
+                  title: """Protagonist (ENFJ)""",
                 ),
               ),
-              //Math Button
-              Padding(
-                padding: EdgeInsets.only(
-                    top: scaler.getHeight(1),
-                    bottom: scaler.getHeight(1),
-                    left: scaler.getHeight(1.5),
-                    right: scaler.getHeight(1.5)),
-                child: MajorButtom(
-                  detail: """Campaigner \n(ENFP)"""
-                      .trim(),
-                  color: color,
-                  image: PersonalityTestInfo().personalityIconsPath['ENFP'],
-                  alignment: Alignment(-.3, 0),
-                  screen: PersonalityTemplate(
-                    desc: CollegesInfo().shawDesc,
-                    imagesList: CollegesInfo().defaultList,
-                    title: """Campaigner (ENFP)""",
-                  ),
+            ),
+            //Math Button
+            Padding(
+              padding: EdgeInsets.only(
+                  top: scaler.getHeight(1),
+                  bottom: scaler.getHeight(1),
+                  left: scaler.getHeight(1.5),
+                  right: scaler.getHeight(1.5)),
+              child: MajorButtom(
+                detail: """Campaigner \n(ENFP)""".trim(),
+                color: color,
+                image: PersonalityTestInfo().personalityIconsPath['ENFP'],
+                alignment: Alignment(-.3, 0),
+                screen: PersonalityTemplate(
+                  desc: CollegesInfo().shawDesc,
+                  imagesList: CollegesInfo().defaultList,
+                  title: """Campaigner (ENFP)""",
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
-        bottomNavigationBar: Container(
-          height: scaler.getHeight(10),
-          decoration: BoxDecoration(gradient: turkish),
-        ),
-      );
-    }
-
+      ),
+      bottomNavigationBar: Container(
+        height: scaler.getHeight(10),
+        decoration: BoxDecoration(gradient: turkish),
+      ),
+    );
+  }
 }
